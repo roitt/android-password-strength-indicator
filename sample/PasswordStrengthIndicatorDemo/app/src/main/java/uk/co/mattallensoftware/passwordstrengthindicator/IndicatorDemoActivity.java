2845856c0cfa30d50d24ab738860fa0473a2abf8
Matthew Allen <allenm@qti.qualcomm.com>
2014-07-01 13:34:02 +0100

    Added early sample
    Change-Id: I747da2e7333f9a57095e41449b388f42805db896

---
+++ sample/PasswordStrengthIndicatorDemo/app/src/main/java/uk/co/mattallensoftware/passwordstrengthindicator/IndicatorDemoActivity.java
@@ -0,0 +1,63 @@
+package uk.co.mattallensoftware.passwordstrengthindicator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+
+public class IndicatorDemoActivity extends Activity {
+
+    private static final String TAG = IndicatorDemoActivity.class.getSimpleName();
+
+    private EditText mPasswordField;
+    //private PasswordStrengthView mPasswordStrength;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indicator_demo);
+
+        mPasswordField = (EditText) findViewById(R.id.password_field);
+        // Listen for text change events
+        mPasswordField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.indicator_demo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ sample/PasswordStrengthIndicatorDemo/app/src/main/java/uk/co/mattallensoftware/passwordstrengthindicator/PasswordStrengthView.java
@@ -0,0 +1,27 @@
+package uk.co.mattallensoftware.passwordstrengthindicator;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Matt Allen
+ * 01/07/14
+ * http://www.mattallensoftware.co.uk
+ * mattallen092@gmail.com
+ *
+ */
+public class PasswordStrengthView extends View {
+
+    public PasswordStrengthView(Context context) {
+        super(context);
+    }
+
+    public PasswordStrengthView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PasswordStrengthView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
